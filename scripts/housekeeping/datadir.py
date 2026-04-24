8d25b5db8e59551e396c72a2ebd24c4c193c56e6
RabidIsGaming <amyparry42@gmail.com>
2026-04-24 23:04:19 +0100

    mod specific save data directory

--- scripts/housekeeping/datadir.py
+++ scripts/housekeeping/datadir.py
@@ -36,7 +36,7 @@ def get_data_dir():
     from platformdirs import user_data_dir
 
 
-    return user_data_dir('ClanGen', 'ClanGen')
+    return user_data_dir('AntlerGen', 'ClanGen')
 
 
 def get_log_dir():
